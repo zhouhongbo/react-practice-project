d1c7e8346afca99bf6c027450720549b5bc12275
10521503 <zhouhb@boe.com.cn>
2021-08-05 17:11:21 +0800

    引入路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
+import { Route, Switch } from 'react-router-dom'
 import './App.less'
+
+import Login from './pages/Login'
+import Admin from './pages/Admin'
 
 export default class App extends Component {
     render() {
         return (
-            <div className="app">
-                <Button>按钮</Button>
-            </div>
+            <>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/" component={Admin}></Route>
+                </Switch>
+            </>
         )
     }
 }

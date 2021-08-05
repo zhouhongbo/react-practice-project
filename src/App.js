911cfe7e35425d4377ad3c8a02e518ad4cfcec09
10521503 <zhouhb@boe.com.cn>
2021-08-05 16:10:00 +0800

    配置antd环境

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import {Button} from 'antd'
 import './App.less'
 
 export default class App extends Component {
     render() {
         return (
             <div className="app">
-                demo
+                <Button>按钮</Button>
             </div>
         )
     }

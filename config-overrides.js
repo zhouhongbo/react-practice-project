4dbc9c9aa3ee94a3e10705dcd263f28f37b4eeef
10521503 <zhouhb@boe.com.cn>
2021-08-05 16:05:55 +0800

    配置less环境

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const {
+    override, addLessLoader } = require('customize-cra');
+module.exports = override(
+    addLessLoader({
+
+        lessOptions: {
+
+            javascriptEnabled: true,
+            localIdentName: '[local]--[hash:base64:5]'
+        }
+    }),
+);
